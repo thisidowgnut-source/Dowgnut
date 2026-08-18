dd143b1c8a43154df64b57f37a36f9d628594d5d
thisidowgnut-source <thisidowgnut@gmail.com>
2026-08-18 17:33:14 +0800

    fix: wire cart provider into app root

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,3 +1,10 @@
 import { Stack } from 'expo-router';
 import { CartProvider } from '../src/CartContext';
-export default function Layout(){return <CartProvider><Stack screenOptions={{headerShown:false}}/></CartProvider>}
+
+export default function Layout() {
+  return (
+    <CartProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </CartProvider>
+  );
+}
